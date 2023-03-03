7e4e7c284dfe15b0fa0aac6be6c528bc641afe15
hnbnh <huynhninhbinh123@gmail.com>
2023-03-03 15:45:16 +0700

    refactor(nvim): update firenvim options

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -2,7 +2,9 @@ return {
   {
     "glacambre/firenvim",
     lazy = false,
+    cond = not not vim.g.started_by_firenvim,
     build = function()
+      require("lazy").load({ plugins = "firenvim", wait = true })
       vim.fn["firenvim#install"](0)
     end,
   },
