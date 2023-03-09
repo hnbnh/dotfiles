72335a6f55c991d9d68aac0102c464c9f52412d7
hnbnh <huynhninhbinh123@gmail.com>
2023-03-09 10:40:54 +0700

    fix(nvim,glow): add missing config option

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -8,7 +8,7 @@ return {
       vim.fn["firenvim#install"](0)
     end,
   },
-  { "ellisonleao/glow.nvim", cmd = "Glow" },
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
   { "terrastruct/d2-vim", ft = { "d2" } },
   { "dstein64/vim-startuptime", cmd = "StartupTime" },
   {
