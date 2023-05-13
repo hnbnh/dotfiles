066dc2678cd6356ceae2c328b2727e438611ab1d
hnbnh <huynhninhbinh123@gmail.com>
2023-05-13 12:16:15 +0700

    refactor: colorful-winsep -> tint

--- config/nvim/lua/plugins/ui/init.lua
+++ config/nvim/lua/plugins/ui/init.lua
@@ -127,13 +127,18 @@ return {
     end,
   },
   {
-    "nvim-zh/colorful-winsep.nvim",
-    event = "WinNew",
+    "levouh/tint.nvim",
+    event = "BufRead",
     config = function()
-      local p = require("kanagawa.colors").setup().palette
+      require("tint").setup({
+        ignore = { "WinSeparator", "Status.*", "IndentBlankline.*", "SignColumn", "EndOfBuffer" }, -- Highlight group patterns to ignore, see `string.find`
+        ignorefunc = function(winid)
+          local bufid = vim.api.nvim_win_get_buf(winid)
+          local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
 
-      require("colorful-winsep").setup({
-        highlight = { bg = p.sumiInk1, fg = p.roninYellow },
+          -- Tint normal buffer
+          return buftype ~= ""
+        end,
       })
     end,
   },
