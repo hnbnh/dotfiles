8f5347c4059a76410943d00ac0f5d5e77abde9fb
hnbnh <huynhninhbinh123@gmail.com>
2023-06-21 22:32:25 +0700

    feat(nvim,colorful-winsep): disable highlight on neotree

--- config/nvim/lua/plugins/ui/init.lua
+++ config/nvim/lua/plugins/ui/init.lua
@@ -173,6 +173,19 @@ return {
   {
     "nvim-zh/colorful-winsep.nvim",
     config = true,
+    opts = {
+      create_event = function()
+        local colorful_winsep = require("colorful-winsep")
+        local win_n = require("colorful-winsep.utils").calculate_number_windows()
+        if win_n == 2 then
+          local win_id = vim.fn.win_getid(vim.fn.winnr("h"))
+          local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win_id), "filetype")
+          if filetype == "neo-tree" then
+            colorful_winsep.NvimSeparatorDel()
+          end
+        end
+      end,
+    },
     event = { "WinNew" },
   },
   { "folke/which-key.nvim" },
