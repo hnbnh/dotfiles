1fc2c6b169f8c17df42b61cf391028fb89c8512b
hnbnh <huynhninhbinh123@gmail.com>
2023-06-22 22:02:38 +0700

    feat(nvim): add flash.nvim

--- config/nvim/lua/plugins/ui/init.lua
+++ config/nvim/lua/plugins/ui/init.lua
@@ -327,12 +327,35 @@ return {
   {
     "jinh0/eyeliner.nvim",
     event = "VeryLazy",
+    enabled = false,
     config = function()
       require("eyeliner").setup({
         highlight_on_key = true, -- show highlights only after keypress
         dim = true, -- dim all other characters if set to true (recommended!)
       })
     end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+      },
+      {
+        "S",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+      },
+    },
   },
   { "pwntester/octo.nvim", cmd = "Octo", config = true },
 }
