336dbe892e95beb0b67b5f74a28c9c184d1df7a8
hnbnh <huynhninhbinh123@gmail.com>
2023-07-15 19:00:07 +0700

    feat(nvim): add project.nvim

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -59,6 +59,7 @@ return {
       telescope.load_extension("fzf")
       telescope.load_extension("ui-select")
       telescope.load_extension("live_grep_args")
+      telescope.load_extension("projects")
     end,
   },
 }
