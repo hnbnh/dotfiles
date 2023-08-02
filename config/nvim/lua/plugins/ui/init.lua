e121e7ef0a9536801fdf6864a6443d85582f4c0b
hnbnh <huynhninhbinh123@gmail.com>
2023-08-02 20:26:45 +0700

    feat(nvim,neotree): migrate to v3

--- config/nvim/lua/plugins/ui/init.lua
+++ config/nvim/lua/plugins/ui/init.lua
@@ -128,7 +128,7 @@ return {
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     cmd = "Neotree",
     config = function()
       require("neo-tree").setup({
