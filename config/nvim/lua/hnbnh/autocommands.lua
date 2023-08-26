59833e1addd46e6c24bd75c951a29c9e3b87a1d5
hnbnh <huynhninhbinh123@gmail.com>
2023-08-26 21:35:22 +0700

    feat(nvim,persistence): autoload session on VimEnter

--- config/nvim/lua/hnbnh/autocommands.lua
+++ config/nvim/lua/hnbnh/autocommands.lua
@@ -3,3 +3,11 @@ vim.cmd(
   [[autocmd FileType help,checkhealth,startuptime,qf,lspinfo,fugitive,fugitiveblame nnoremap <buffer><silent> q :close<CR>]]
 )
 vim.cmd([[autocmd FileType man nnoremap <buffer><silent> q :quit<CR>]])
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("PersistenceRestore", { clear = true }),
+  callback = function()
+    require("persistence").load()
+  end,
+  nested = true,
+})
