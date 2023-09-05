6486d9e15c2011030326d1a1598e2b9d6ad73881
hnbnh <huynhninhbinh123@gmail.com>
2023-09-05 13:59:06 +0700

    feat(nvim): add ChatGPT

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -1,3 +1,5 @@
+local home = vim.fn.expand("$HOME")
+
 return {
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
   { "terrastruct/d2-vim", ft = { "d2" } },
@@ -12,6 +14,14 @@ return {
     version = "2.*",
     opts = {
       hint = "floating-big-letter",
+    },
+    config = true,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    opts = {
+      api_key_cmd = "gpg --decrypt " .. home .. "/openai_api_key.txt.gpg",
     },
     config = true,
   },
--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -88,5 +88,24 @@ return {
       r = { name = "+refactor", p = { "<cmd>lua require('refactoring').select_refactor()<cr>", "Refactor" } },
       g = { name = "+git", b = { ":GBrowse!<cr>", "Git browse" } },
     }, { mode = "v", prefix = "<leader>" })
+
+    wk.register({
+      c = {
+        name = "ChatGPT",
+        c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+        e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
+        g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
+        t = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
+        k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
+        d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
+        a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
+        o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
+        s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
+        f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
+        x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
+        r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
+        l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
+      },
+    }, { mode = { "n", "v" }, prefix = "<leader>" })
   end,
 }
