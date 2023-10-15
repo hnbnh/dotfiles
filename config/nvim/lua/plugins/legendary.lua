a532d94aa88fbe5ff5fe672e85cbd83e4add62f7
hnbnh <huynhninhbinh123@gmail.com>
2023-10-15 20:07:48 +0700

    feat(nvim): add legendary.nvim

---
+++ config/nvim/lua/plugins/legendary.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mrjones2014/legendary.nvim",
+    priority = 10000,
+    lazy = false,
+    dependencies = { "kkharji/sqlite.lua" },
+    opts = {
+      extensions = {
+        lazy_nvim = true,
+      },
+      commands = {},
+    },
+  },
+}
