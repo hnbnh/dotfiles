7df83cbed97a01b5b4c0d63021a73757673665ee
hnbnh <huynhninhbinh123@gmail.com>
2023-10-17 22:24:44 +0700

    feat(nvim): add edgy.nvim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -448,4 +448,58 @@ return {
     },
     event = "VeryLazy",
   },
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {
+      animate = {
+        enabled = false,
+      },
+      bottom = {
+        "Trouble",
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- don't open help files in edgy that we're editing
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
+        { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+      },
+      left = {
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          pinned = true,
+          size = { height = 0.5 },
+        },
+        { title = "Neotest Summary", ft = "neotest-summary" },
+        {
+          title = "Neo-Tree Git",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "git_status"
+          end,
+          pinned = true,
+          open = "Neotree position=right git_status",
+        },
+        {
+          title = "Neo-Tree Buffers",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        "neo-tree",
+      },
+    },
+  },
 }
