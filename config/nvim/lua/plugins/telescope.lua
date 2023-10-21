42612a2c0316b3f210b21041ba896544fb6ab515
hnbnh <huynhninhbinh123@gmail.com>
2023-10-22 00:32:39 +0700

    feat(nvim,telescope): smart_case -> ignore_case

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -51,7 +51,7 @@ return {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
-            case_mode = "smart_case",
+            case_mode = "ignore_case",
           },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({
