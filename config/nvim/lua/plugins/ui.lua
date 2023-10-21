b160817cd1ce6fcc216affc9aced2e906f51ca19
hnbnh <huynhninhbinh123@gmail.com>
2023-10-21 18:01:58 +0700

    feat(nvim,noice): disable progress, popupmenu & smartmove

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -403,10 +403,8 @@ return {
     event = "VeryLazy",
     opts = {
       lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
+        progress = {
+          enabled = false,
         },
         hover = {
           enabled = false,
@@ -427,10 +425,10 @@ return {
         enabled = false,
       },
       popupmenu = {
-        enabled = true,
+        enabled = false,
       },
       smart_move = {
-        enabled = true,
+        enabled = false,
       },
     },
   },
