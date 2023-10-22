082ab647335fdecff2aff65da945205e3d83dffc
hnbnh <huynhninhbinh123@gmail.com>
2023-10-22 17:01:46 +0700

    feat(nvim,legendary): add copy file path commands

--- config/nvim/lua/plugins/legendary.lua
+++ config/nvim/lua/plugins/legendary.lua
@@ -8,7 +8,29 @@ return {
       extensions = {
         lazy_nvim = true,
       },
-      commands = {},
+      commands = {
+        {
+          ":CopyFileName",
+          function()
+            vim.cmd("let @+ = expand('%')")
+            vim.notify("Copied file name to clipboard")
+          end,
+        },
+        {
+          ":CopyAbsoluteFilePath",
+          function()
+            vim.cmd("let @+ = expand('%:p')")
+            vim.notify("Copied ABSOLUTE file path to clipboard")
+          end,
+        },
+        {
+          ":CopyRelativeFilePath",
+          function()
+            vim.cmd("let @+ = expand('%:p:.')")
+            vim.notify("Copied RELATIVE file path to clipboard")
+          end,
+        },
+      },
     },
   },
 }
