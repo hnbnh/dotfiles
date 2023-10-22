d11516ab56e4052ceb98417f1b48b551819b12db
hnbnh <huynhninhbinh123@gmail.com>
2023-10-22 12:07:07 +0700

    feat(nvim,telescope): borderless theme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,20 @@ return {
           return {
             SatelliteSearch = { fg = colors.overlay0, bg = colors.peach },
             SpellBad = { fg = colors.crust, bg = colors.red },
+
+            TelescopeMatching = { fg = colors.flamingo },
+            TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+            TelescopePreviewNormal = { bg = colors.mantle },
+            TelescopePreviewTitle = { bg = colors.teal, fg = colors.mantle },
+            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+            TelescopePromptNormal = { bg = colors.surface0 },
+            TelescopePromptPrefix = { bg = colors.surface0 },
+            TelescopePromptTitle = { bg = colors.maroon, fg = colors.mantle },
+            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+            TelescopeResultsNormal = { bg = colors.mantle },
+            TelescopeResultsTitle = { fg = colors.mantle },
+            TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
             WinSeparator = { fg = colors.overlay0 },
             YankyPut = { fg = colors.crust, bg = colors.green },
             YankyYanked = { fg = colors.crust, bg = colors.yellow },
