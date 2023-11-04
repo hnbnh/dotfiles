3b4b00b06cf5800ffbb9d99cfc8998e3f7fcd964
hnbnh <huynhninhbinh123@gmail.com>
2023-11-04 23:01:47 +0700

    feat(nvim): add toggleterm.nvim

--- config/nvim/lua/hnbnh/utils.lua
+++ config/nvim/lua/hnbnh/utils.lua
@@ -1,43 +1,54 @@
-local M = {}
-
--- HACK: https://github.com/hrsh7th/nvim-cmp/issues/1017#issuecomment-1141440976
-table.unpack = table.unpack or unpack -- Lua 5.1 compatibility
-
-M.join = function(...)
-  local merged = {}
-  for _, t in ipairs({ ... }) do
-    for _, v in pairs(t) do
-      table.insert(merged, v)
+return {
+  join = function(...)
+    local merged = {}
+    for _, t in ipairs({ ... }) do
+      for _, v in pairs(t) do
+        table.insert(merged, v)
+      end
     end
-  end
 
-  return merged
-end
+    return merged
+  end,
 
-M.toggle_spell = function()
-  vim.opt.spell = not (vim.opt.spell:get())
-end
+  toggle_spell = function()
+    vim.opt.spell = not (vim.opt.spell:get())
+  end,
 
-M.fg = function(name)
-  ---@type {foreground?:number}?
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-  local fg = hl and hl.fg or hl.foreground
-  return fg and { fg = string.format("#%06x", fg) }
-end
+  fg = function(name)
+    ---@type {foreground?:number}?
+    local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name })
+      or vim.api.nvim_get_hl_by_name(name, true)
+    local fg = hl and hl.fg or hl.foreground
+    return fg and { fg = string.format("#%06x", fg) }
+  end,
 
-M.dump = function(...)
-  local objects = vim.tbl_map(vim.inspect, { ... })
-  vim.notify(table.unpack(objects))
-end
+  dump = function(...)
+    local objects = vim.tbl_map(vim.inspect, { ... })
+    vim.notify(table.unpack(objects))
+  end,
 
-M.duplicate = function(s, n)
-  local result = ""
+  duplicate = function(s, n)
+    local result = ""
 
-  for _ = 1, n do
-    result = result .. s
-  end
+    for _ = 1, n do
+      result = result .. s
+    end
 
-  return result
-end
+    return result
+  end,
 
-return M
+  Singleton = function(instance_callback)
+    local obj = {}
+
+    function obj.new()
+      if obj.instance then
+        return obj.instance
+      end
+
+      obj.instance = instance_callback()
+      return obj.instance
+    end
+
+    return obj
+  end,
+}
--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -1,4 +1,31 @@
+local utils = require("hnbnh.utils")
+
 local home = vim.fn.expand("$HOME")
+
+local function Terminal(cmd, opts)
+  local term_opts = vim.tbl_deep_extend("force", {
+    cmd = cmd,
+    direction = "float",
+    hidden = true,
+    on_create = function(term)
+      vim.keymap.set("n", "q", function()
+        term:close()
+      end, { buffer = term.bufnr, silent = true })
+    end,
+  }, opts or {})
+
+  return utils.Singleton(function()
+    return require("toggleterm.terminal").Terminal:new(term_opts)
+  end)
+end
+
+local Lazygit = Terminal("lazygit", {
+  on_close = function()
+    require("neo-tree.sources.git_status").refresh()
+  end,
+})
+local Rails_c = Terminal("rails c")
+local FloatTerm = Terminal()
 
 return {
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
@@ -29,5 +56,37 @@ return {
     "Dhanus3133/LeetBuddy.nvim",
     cmd = { "LBQuestions", "LBQuestion", "LBReset", "LBTest", "LBSubmit" },
     config = true,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    keys = {
+      {
+        "<esc>",
+        "<c-\\><c-n>",
+        desc = "Close terminal",
+        mode = "t",
+      },
+      {
+        "<c-t>t",
+        function()
+          FloatTerm.new():toggle()
+        end,
+        desc = "Open floating terminal",
+      },
+      {
+        "<c-g>",
+        function()
+          Lazygit.new():toggle()
+        end,
+        desc = "Open lazygit",
+      },
+      {
+        "<c-t>c",
+        function()
+          Rails_c.new():toggle()
+        end,
+        desc = "Open rails console",
+      },
+    },
   },
 }
