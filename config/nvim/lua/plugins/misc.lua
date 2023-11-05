64dfa73040809bfbae071c2f21c971965a0be9b0
hnbnh <huynhninhbinh123@gmail.com>
2023-11-05 12:09:36 +0700

    feat(nvim,chatgpt): load chatgpt on keymaps

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -46,7 +46,27 @@ return {
   },
   {
     "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
+    keys = {
+      { "<leader>cc", "<cmd>ChatGPT<cr>", desc = "ChatGPT", mode = { "n", "v" } },
+      { "<leader>ce", "<cmd>ChatGPTEditWithInstruction<cr>", desc = "Edit with instruction", mode = { "n", "v" } },
+      { "<leader>cg", "<cmd>ChatGPTRun grammar_correction<cr>", desc = "Grammar Correction", mode = { "n", "v" } },
+      { "<leader>ct", "<cmd>ChatGPTRun translate<cr>", desc = "Translate", mode = { "n", "v" } },
+      { "<leader>ck", "<cmd>ChatGPTRun keywords<cr>", desc = "Keywords", mode = { "n", "v" } },
+      { "<leader>cd", "<cmd>ChatGPTRun docstring<cr>", desc = "Docstring", mode = { "n", "v" } },
+      { "<leader>ca", "<cmd>ChatGPTRun add_tests<cr>", desc = "Add Tests", mode = { "n", "v" } },
+      { "<leader>co", "<cmd>ChatGPTRun optimize_code<cr>", desc = "Optimize Code", mode = { "n", "v" } },
+      { "<leader>cs", "<cmd>ChatGPTRun summarize<cr>", desc = "Summarize", mode = { "n", "v" } },
+      { "<leader>cf", "<cmd>ChatGPTRun fix_bugs<cr>", desc = "Fix Bugs", mode = { "n", "v" } },
+      { "<leader>cx", "<cmd>ChatGPTRun explain_code<cr>", desc = "Explain Code", mode = { "n", "v" } },
+      { "<leader>cr", "<cmd>ChatGPTRun roxygen_edit<cr>", desc = "Roxygen Edit", mode = { "n", "v" } },
+      {
+        "<leader>cl",
+        "<cmd>ChatGPTRun code_readability_analysis<cr>",
+        desc = "Code Readability Analysis",
+        mode = { "n", "v" },
+      },
+    },
+
     opts = {
       api_key_cmd = "gpg --decrypt " .. home .. "/openai_api_key.txt.gpg",
     },
--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -93,25 +93,6 @@ return {
     }, { mode = "v", prefix = "<leader>" })
 
     wk.register({
-      c = {
-        name = "ChatGPT",
-        c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-        e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
-        g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
-        t = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
-        k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
-        d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
-        a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
-        o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
-        s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
-        f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
-        x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
-        r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
-        l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
-      },
-    }, { mode = { "n", "v" }, prefix = "<leader>" })
-
-    wk.register({
       s = { ":g//normal @i" .. utils.duplicate("<left>", 10), "Search and apply macro" },
       a = { ":normal @i<cr>", "Apply macro" },
     }, { mode = "x", prefix = "<leader>" })
