8278cdf2c912779f27ecb48a143179252aba6c0a
hnbnh <huynhninhbinh123@gmail.com>
2023-11-05 20:21:43 +0700

    refactor(nvim,vim-visual-multi): remove custom keymaps

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -205,12 +205,6 @@ return {
   },
   {
     "mg979/vim-visual-multi",
-    init = function()
-      vim.g.VM_maps = {
-        ["Find Under"] = "<C-d>",
-        ["Find Subword Under"] = "<C-d>",
-      }
-    end,
     event = "BufReadPost",
     config = true,
   },
