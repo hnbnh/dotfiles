c7bdd86faa676d807dae18e61a434991770c2d34
hnbnh <huynhninhbinh123@gmail.com>
2024-02-03 22:47:45 +0700

    feat(wezterm): add more icons

--- config/wezterm/icons.lua
+++ config/wezterm/icons.lua
@@ -1,35 +1,49 @@
+local wezterm = require("wezterm")
 local colors = require("colors").get_current_colors()
+local nerdfonts = wezterm.nerdfonts
 
 local M = {}
 
 local icons = {
 	cargo = {
 		color = colors.peach,
-		text = "",
+		text = nerdfonts.dev_rust,
 	},
 	docker = {
 		color = colors.blue,
-		text = "󰡨",
+		text = nerdfonts.linux_docker,
+	},
+	["docker-compose"] = {
+		color = colors.blue,
+		text = nerdfonts.linux_docker,
 	},
 	fallback = {
 		color = colors.text,
-		text = "",
+		text = nerdfonts.dev_terminal,
+	},
+	git = {
+		color = colors.green,
+		text = nerdfonts.dev_git,
 	},
 	go = {
 		color = colors.blue,
-		text = "",
+		text = nerdfonts.seti_go,
 	},
 	lazygit = {
 		color = colors.red,
-		text = "",
+		text = nerdfonts.dev_git_branch,
 	},
 	node = {
 		color = colors.green,
-		text = "󰋘",
+		text = nerdfonts.md_nodejs,
 	},
 	nvim = {
 		color = colors.green,
-		text = "",
+		text = nerdfonts.custom_neovim,
+	},
+	ruby = {
+		color = colors.red,
+		text = nerdfonts.dev_ruby,
 	},
 }
 
