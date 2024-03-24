4e3b825adc17eabc61df0a8fc54f12df18ef9ef0
hnbnh <huynhninhbinh123@gmail.com>
2024-03-24 22:45:55 +0700

    feat(git): more aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -15,6 +15,11 @@ alias tzsh='time zsh -i -c exit'
 alias gst='git status'
 alias gco='git checkout'
 alias gcm='git commit -m'
+alias gf='git fetch'
+alias gmc='git merge --continue'
+alias gma='git merge --abort'
+alias grc='git rebase --continue'
+alias gra='git rebase --abort'
 
 alias fsd='flameshot gui -d 2000'
 
