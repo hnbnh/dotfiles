8e26cdc3f83e7dff4e707df5b7635dc387ba94be
hnbnh <huynhninhbinh123@gmail.com>
2024-09-03 15:46:59 +0700

    feat(nvim): update harpoon, noice & which-key config

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -180,4 +180,42 @@ return {
       },
     },
   },
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      -- stylua: ignore
+      { "<leader>h", function() toggle_telescope(require("harpoon"):list()) end, desc = "Open harpoon window" },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = { format = { lua = false } },
+      lsp = {
+        progress = { enabled = false },
+        hover = { enabled = false },
+        signature = { enabled = false },
+      },
+      preset = {
+        bottom_search = true,
+        long_message_to_split = true,
+        inc_rename = false,
+      },
+      messages = { enabled = false },
+      notify = { enabled = false },
+      popupmenu = { enabled = false },
+      smart_move = { enabled = false },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts_extend = { "spec" },
+    opts = {
+      spec = {
+        {
+          { "<leader>b", group = nil },
+        },
+      },
+    },
+  },
 }
