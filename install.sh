cf16fd92e481c0f3d516d778fbca26aa0b109716
hnbnh <huynhninhbinh123@gmail.com>
2024-12-27 10:26:22 +0700

    style: reformat install.sh

--- install.sh
+++ install.sh
@@ -3,72 +3,72 @@
 set -e
 
 function have {
-    command -v "$1" &> /dev/null
+  command -v "$1" &>/dev/null
 }
 
 function install_dots {
-    CFG_DIR="$HOME/dotfiles/config"
-    DEST_DIR="$HOME/.config"
+  CFG_DIR="$HOME/dotfiles/config"
+  DEST_DIR="$HOME/.config"
 
-    mkdir -p $DEST_DIR
+  mkdir -p $DEST_DIR
 
-    for dir in "$CFG_DIR"/*/; do
-        ln -sfnv $dir $DEST_DIR
-    done
+  for dir in "$CFG_DIR"/*/; do
+    ln -sfnv $dir $DEST_DIR
+  done
 
-    ln -sfnv "$CFG_DIR/zsh/.zshrc" "$HOME/.zshrc"
-    ln -sfnv "$CFG_DIR/zsh/.zimrc" "$HOME/.zimrc"
-    ln -sfnv "$CFG_DIR/git/.gitconfig" "$HOME/.gitconfig"
-    ln -sfnv "$CFG_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
-    ln -sfnv "$CFG_DIR/betterlockscreen/betterlockscreenrc" "$HOME/.config/betterlockscreenrc"
-    ln -sfnv "$CFG_DIR/ripgrep/.ripgreprc" "$HOME/.ripgreprc"
-    ln -sfnv "$CFG_DIR/yt-dlp/yt-dlp.conf" "$HOME/yt-dlp.conf"
+  ln -sfnv "$CFG_DIR/zsh/.zshrc" "$HOME/.zshrc"
+  ln -sfnv "$CFG_DIR/zsh/.zimrc" "$HOME/.zimrc"
+  ln -sfnv "$CFG_DIR/git/.gitconfig" "$HOME/.gitconfig"
+  ln -sfnv "$CFG_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+  ln -sfnv "$CFG_DIR/betterlockscreen/betterlockscreenrc" "$HOME/.config/betterlockscreenrc"
+  ln -sfnv "$CFG_DIR/ripgrep/.ripgreprc" "$HOME/.ripgreprc"
+  ln -sfnv "$CFG_DIR/yt-dlp/yt-dlp.conf" "$HOME/yt-dlp.conf"
 }
 
 function setup_linux {
-    # Install ansible & plugins
-    have ansible || sudo dnf install ansible -y
+  # Install ansible & plugins
+  have ansible || sudo dnf install ansible -y
 
-    [ -d ~/.ansible/collections/ansible_collections/community ] || \
-        ansible-galaxy collection install community.general
+  [ -d ~/.ansible/collections/ansible_collections/community ] ||
+    ansible-galaxy collection install community.general
 
-    # Run Ansible
-    ansible-playbook -i ./ansible/hosts ./ansible/linux.yml --ask-become-pass
+  # Run Ansible
+  ansible-playbook -i ./ansible/hosts ./ansible/linux.yml --ask-become-pass
 }
 
 function setup_macos {
-    # Install nix
-    if ! have "nix-build"; then
-        curl -L https://nixos.org/nix/install | sh
-        # Workaround for https://github.com/LnL7/nix-darwin/issues/149
-        sudo rm /etc/nix/nix.conf
-    fi
+  # Install nix
+  if ! have "nix-build"; then
+    curl -L https://nixos.org/nix/install | sh
+    # Workaround for https://github.com/LnL7/nix-darwin/issues/149
+    sudo rm /etc/nix/nix.conf
+  fi
 
-    # Install homebrew
-    if ! have brew; then
-        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o /tmp/brew-install.sh
-        NONINTERACTIVE=1 sudo -u $USERNAME bash /tmp/brew-install.sh
-    fi
+  # Install homebrew
+  if ! have brew; then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o /tmp/brew-install.sh
+    NONINTERACTIVE=1 sudo -u $USERNAME bash /tmp/brew-install.sh
+  fi
 
-    # Set up nix-darwin
-    cd nix-darwin
-    /nix/var/nix/profiles/default/bin/nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-    yes | ./result/bin/darwin-installer
+  # Set up nix-darwin
+  cd nix-darwin
+  /nix/var/nix/profiles/default/bin/nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+  yes | ./result/bin/darwin-installer
 
-    /nix/var/nix/profiles/default/bin/nix --experimental-features 'nix-command flakes' build ./\#darwinConfigurations.$(hostname -s).system
-    ./result/sw/bin/darwin-rebuild switch --flake .
+  /nix/var/nix/profiles/default/bin/nix --experimental-features 'nix-command flakes' build ./\#darwinConfigurations.$(hostname -s).system
+  ./result/sw/bin/darwin-rebuild switch --flake .
 }
 
 function main() {
-    if [ "$(uname)" == "Darwin" ]; then
-        install_dots
-        setup_macos
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        setup_linux
-    else
-        echo "Unsupported OS"
-        exit 1
-    fi
+  if [ "$(uname)" == "Darwin" ]; then
+    install_dots
+    setup_macos
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    setup_linux
+  else
+    echo "Unsupported OS"
+    exit 1
+  fi
 }
 
 main
