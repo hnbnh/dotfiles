e4cdf3a0577d3c0e74b595dbaa7fd1d0086421af
hnbnh <huynhninhbinh123@gmail.com>
2024-12-31 11:52:14 +0700

    feat(nvim): disable auto checker

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
+    enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
