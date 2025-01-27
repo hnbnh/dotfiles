1bc170100bf58d01b002498b6b9adbba1d306e19
hnbnh <huynhninhbinh123@gmail.com>
2025-01-27 10:19:52 +0700

    feat(nvim): enable bufferline.nvim

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "folke/todo-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
   { "ibhagwan/fzf-lua", enabled = false },
   { "saghen/blink.cmp", enabled = false },
 }
