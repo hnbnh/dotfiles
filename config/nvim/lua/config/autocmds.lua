40c9211c44e64615d144c10777572791cd221d21
hnbnh <huynhninhbinh123@gmail.com>
2025-02-13 22:07:50 +0700

    feat(nvim): load edgy on VimEnter

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -30,6 +30,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
   group = augroup("PersistenceRestore"),
   callback = function()
     if vim.fn.argv(0, -1) == "." then
+      require("edgy").open()
       require("persistence").load()
     end
   end,
