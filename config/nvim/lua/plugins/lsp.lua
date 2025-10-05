9f5450e90a1516264cbb3196f71bcba661dac9ee
hnbnh <huynhninhbinh123@gmail.com>
2025-10-05 19:29:51 +0700

    refactor(nvim): improve LSP configuration structure

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -6,30 +6,29 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<c-k>", false }
 
-      return opts
-    end,
-    opts = {
-      -- options for vim.diagnostic.config()
-      ---@type vim.diagnostic.Opts
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_lines = { current_line = true },
-      },
-      servers = {
-        ts_ls = { enabled = false },
-        solargraph = {
-          enabled = false,
-          init_options = {
-            autoformat = false,
-            formatting = false,
-          },
+      return vim.tbl_deep_extend("force", opts, {
+        -- options for vim.diagnostic.config()
+        ---@type vim.diagnostic.Opts
+        diagnostics = {
+          underline = true,
+          update_in_insert = false,
+          virtual_lines = { current_line = true },
         },
-        ruby_lsp = { enabled = false },
-        typos_lsp = {},
-      },
-      inlay_hints = { enabled = false },
-    },
+        servers = {
+          ts_ls = { enabled = false },
+          solargraph = {
+            enabled = false,
+            init_options = {
+              autoformat = false,
+              formatting = false,
+            },
+          },
+          ruby_lsp = { enabled = false },
+          typos_lsp = {},
+        },
+        inlay_hints = { enabled = false },
+      })
+    end,
   },
   {
     "mason-org/mason.nvim",
