d14cc9c9b712946aa93ca4a0c8bf543bae5f5c51
hnbnh <huynhninhbinh123@gmail.com>
2025-11-10 18:25:45 +0700

    refactor(nvim,lsp): replace deprecated keymaps.get() with keys field

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,33 +2,33 @@ return {
   {
     "neovim/nvim-lspconfig",
     cond = not vim.g.vscode,
-    opts = function(_, opts)
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<c-k>", false }
-
-      return vim.tbl_deep_extend("force", opts, {
-        -- options for vim.diagnostic.config()
-        ---@type vim.diagnostic.Opts
-        diagnostics = {
-          underline = true,
-          update_in_insert = false,
-          virtual_lines = { current_line = true },
-        },
-        servers = {
-          ts_ls = { enabled = false },
-          solargraph = {
-            enabled = false,
-            init_options = {
-              autoformat = false,
-              formatting = false,
-            },
+    opts = {
+      -- options for vim.diagnostic.config()
+      ---@type vim.diagnostic.Opts
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_lines = { current_line = true },
+      },
+      servers = {
+        ["*"] = {
+          keys = {
+            { "<c-k>", false },
           },
-          ruby_lsp = { enabled = false },
-          typos_lsp = {},
         },
-        inlay_hints = { enabled = false },
-      })
-    end,
+        ts_ls = { enabled = false },
+        solargraph = {
+          enabled = false,
+          init_options = {
+            autoformat = false,
+            formatting = false,
+          },
+        },
+        ruby_lsp = { enabled = false },
+        typos_lsp = {},
+      },
+      inlay_hints = { enabled = false },
+    },
   },
   {
     "mason-org/mason.nvim",
