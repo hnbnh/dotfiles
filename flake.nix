748f5fbc6d5de5921839ac289e5a3115feee4281
hnbnh <huynhninhbinh123@gmail.com>
2026-08-21 00:04:57 +0700

    feat(nix): declare numtide's cache in the flake's nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "hnbnh's workspace";
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin = {
