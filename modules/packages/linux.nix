f57df54f0cf837284440b1e5e11303003cff597c
hnbnh <huynhninhbinh123@gmail.com>
2026-09-08 07:31:47 +0700

    chore(packages): drop llvmpipe wrapper for ghostty and kitty

--- modules/packages/linux.nix
+++ modules/packages/linux.nix
@@ -1,26 +1,12 @@
 { pkgs, ... }:
 
-let
-  # virtio-gpu/virgl in the Linux guest exposes no desktop OpenGL core profile.
-  # Use llvmpipe only for affected applications, leaving other apps on the real GPU.
-  softwareGL = pkg: pkgs.symlinkJoin {
-    name = "${pkg.pname}-software-gl";
-    paths = [ pkg ];
-    passthru = { inherit (pkg) pname version; };
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/${pkg.meta.mainProgram} \
-        --set-default LIBGL_ALWAYS_SOFTWARE 1
-    '';
-  };
-in
 {
   home.packages = with pkgs; [
-    (softwareGL ghostty)
-    (softwareGL kitty)
     calibre # marked broken on aarch64-darwin, so Linux-only
+    ghostty
     inter # Noctalia's UI font; Fedora does not ship it
     keyd
+    kitty
     obs-studio
     papirus-icon-theme
     rofi
